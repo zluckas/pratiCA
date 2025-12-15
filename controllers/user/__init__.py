288aa138333304a970871181053c37e29671f3cc
Maria71427 <mspe725@gmail.com>
2025-12-15 11:46:29 -0300

    cadastro aluno x professor

--- controllers/user/__init__.py
+++ controllers/user/__init__.py
@@ -10,8 +10,42 @@ from datetime import date
 usuarios_bp = Blueprint('usuario',__name__, static_folder="static", template_folder="templates")
 
 
-@usuarios_bp.route('/cadastro', methods = ['POST', 'GET'])
+@usuarios_bp.route('/cadastro_usuario', methods = ['POST', 'GET'])
 def cadastro_usuario():
+    if request.method == 'POST':
+        matricula = int(request.form['matricula'])
+        nome = request.form['nome']
+        senha = request.form['senha']
+        categoria = request.form['categoria']
+
+        # Verifica se o email já existe
+        with Session(bind=engine) as session:
+            usuario_existente = session.query(Usuarios).filter_by(matricula=matricula).first()
+            if usuario_existente:
+                flash("Matricula já cadastrada!", "error")
+                return redirect(url_for('usuario.cadastro_usuario'))
+
+          
+            senha_hash = generate_password_hash(senha)
+
+            
+            usuario = Usuarios(
+                matricula=matricula,
+                nome=nome,
+                senha=senha_hash,
+                categoria=categoria
+            )
+
+            session.add(usuario)
+            session.commit()
+
+            flash("Usuário cadastrado com sucesso! Faça login.", "sucesso")
+            return redirect(url_for('auth.login'))
+
+    return render_template('cadastro_usuario.html')
+
+@usuarios_bp.route('/cadastro_aluno', methods = ['POST', 'GET'])
+def cadastro_aluno():
     if request.method == 'POST':
         matricula = int(request.form['matricula'])
         nome = request.form['nome']
@@ -50,9 +84,14 @@ def cadastro_usuario():
 
     with Session(bind=engine) as session:
         cursos = session.query(Cursos).all()
-    return render_template('cadastro_usuario.html', cursos=cursos)
+    return render_template('cadastro_aluno.html', cursos=cursos)
 
 
+@usuarios_bp.route('/cadastro_professor', methods = ['POST', 'GET'])
+def cadastro_professor():   
+   
+
+    return render_template('cadastro_professor.html')
 @usuarios_bp.route('/dashboard')
 @login_required
 def dashboard():
